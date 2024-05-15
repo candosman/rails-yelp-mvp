7fdd5a690ba13827dc96dddbc8cbc9f641e741a4
Bryan Chuttoo <chuttoobryan04@gmail.com>
2024-05-15 11:33:11 +0400

    add review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_15_072521) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_073217) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.integer "phone_number"
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
